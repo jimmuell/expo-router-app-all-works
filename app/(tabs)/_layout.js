1dcb139e77f2314185159eacb8c73521edd19dad
James L Mueller <jamesmueller@liongatetechnology.com>
2023-12-20 14:36:37 -0600

    update 1

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { MaterialCommunityIcons } from "react-native-vector-icons";
 
 export default function TabsLayout() {
-  
   return (
     <Tabs>
       <Tabs.Screen
@@ -11,6 +11,9 @@ export default function TabsLayout() {
         options={{
           headerTitle: "Home",
           title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -18,6 +21,9 @@ export default function TabsLayout() {
         options={{
           headerTitle: "User Page",
           title: "Users",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -25,6 +31,9 @@ export default function TabsLayout() {
         options={{
           headerShown: false,
           title: "Weights",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="weight" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -32,6 +41,9 @@ export default function TabsLayout() {
         options={{
           headerShown: false,
           title: "Menu",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="menu" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
